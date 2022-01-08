52d283f3570ebc16605accc90020ca704b842174
misiekii123 <92684568+misiekii123@users.noreply.github.com>
2022-01-08 17:46:06 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace arduino
+{
+    partial class Arduino
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arduino));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.openserialport = new System.Windows.Forms.Button();
+            this.Info = new System.Windows.Forms.Label();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.portyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wybierzPortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM6ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM7ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM9ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOM10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.COM = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // openserialport
+            // 
+            this.openserialport.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.openserialport.Location = new System.Drawing.Point(11, 28);
+            this.openserialport.Name = "openserialport";
+            this.openserialport.Size = new System.Drawing.Size(267, 45);
+            this.openserialport.TabIndex = 0;
+            this.openserialport.Text = "Open SerialPort";
+            this.openserialport.UseVisualStyleBackColor = true;
+            this.openserialport.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.Info.Location = new System.Drawing.Point(284, 34);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(106, 39);
+            this.Info.TabIndex = 1;
+            this.Info.Text = "Close";
+            this.Info.Click += new System.EventHandler(this.Info_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Arduino SerialPort monitor";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 28);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.portyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1221, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // portyToolStripMenuItem
+            // 
+            this.portyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wybierzPortToolStripMenuItem});
+            this.portyToolStripMenuItem.Name = "portyToolStripMenuItem";
+            this.portyToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.portyToolStripMenuItem.Text = "Porty";
+            // 
+            // wybierzPortToolStripMenuItem
+            // 
+            this.wybierzPortToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOM1ToolStripMenuItem,
+            this.cOM2ToolStripMenuItem,
+            this.cOM3ToolStripMenuItem,
+            this.cOM4ToolStripMenuItem,
+            this.cOM5ToolStripMenuItem,
+            this.cOM6ToolStripMenuItem,
+            this.cOM7ToolStripMenuItem,
+            this.cOM8ToolStripMenuItem,
+            this.cOM9ToolStripMenuItem,
+            this.cOM10ToolStripMenuItem});
+            this.wybierzPortToolStripMenuItem.Name = "wybierzPortToolStripMenuItem";
+            this.wybierzPortToolStripMenuItem.Size = new System.Drawing.Size(178, 26);
+            this.wybierzPortToolStripMenuItem.Text = "Wybierz port";
+            // 
+            // cOM1ToolStripMenuItem
+            // 
+            this.cOM1ToolStripMenuItem.Name = "cOM1ToolStripMenuItem";
+            this.cOM1ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM1ToolStripMenuItem.Text = "COM1";
+            this.cOM1ToolStripMenuItem.Click += new System.EventHandler(this.cOM1ToolStripMenuItem_Click);
+            // 
+            // cOM2ToolStripMenuItem
+            // 
+            this.cOM2ToolStripMenuItem.Name = "cOM2ToolStripMenuItem";
+            this.cOM2ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM2ToolStripMenuItem.Text = "COM2";
+            this.cOM2ToolStripMenuItem.Click += new System.EventHandler(this.cOM2ToolStripMenuItem_Click);
+            // 
+            // cOM3ToolStripMenuItem
+            // 
+            this.cOM3ToolStripMenuItem.Name = "cOM3ToolStripMenuItem";
+            this.cOM3ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM3ToolStripMenuItem.Text = "COM3";
+            this.cOM3ToolStripMenuItem.Click += new System.EventHandler(this.cOM3ToolStripMenuItem_Click);
+            // 
+            // cOM4ToolStripMenuItem
+            // 
+            this.cOM4ToolStripMenuItem.Name = "cOM4ToolStripMenuItem";
+            this.cOM4ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM4ToolStripMenuItem.Text = "COM4";
+            this.cOM4ToolStripMenuItem.Click += new System.EventHandler(this.cOM4ToolStripMenuItem_Click);
+            // 
+            // cOM5ToolStripMenuItem
+            // 
+            this.cOM5ToolStripMenuItem.Name = "cOM5ToolStripMenuItem";
+            this.cOM5ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM5ToolStripMenuItem.Text = "COM5";
+            this.cOM5ToolStripMenuItem.Click += new System.EventHandler(this.cOM5ToolStripMenuItem_Click);
+            // 
+            // cOM6ToolStripMenuItem
+            // 
+            this.cOM6ToolStripMenuItem.Name = "cOM6ToolStripMenuItem";
+            this.cOM6ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM6ToolStripMenuItem.Text = "COM6";
+            this.cOM6ToolStripMenuItem.Click += new System.EventHandler(this.cOM6ToolStripMenuItem_Click);
+            // 
+            // cOM7ToolStripMenuItem
+            // 
+            this.cOM7ToolStripMenuItem.Name = "cOM7ToolStripMenuItem";
+            this.cOM7ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM7ToolStripMenuItem.Text = "COM7";
+            this.cOM7ToolStripMenuItem.Click += new System.EventHandler(this.cOM7ToolStripMenuItem_Click);
+            // 
+            // cOM8ToolStripMenuItem
+            // 
+            this.cOM8ToolStripMenuItem.Name = "cOM8ToolStripMenuItem";
+            this.cOM8ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM8ToolStripMenuItem.Text = "COM8";
+            this.cOM8ToolStripMenuItem.Click += new System.EventHandler(this.cOM8ToolStripMenuItem_Click);
+            // 
+            // cOM9ToolStripMenuItem
+            // 
+            this.cOM9ToolStripMenuItem.Name = "cOM9ToolStripMenuItem";
+            this.cOM9ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM9ToolStripMenuItem.Text = "COM9";
+            this.cOM9ToolStripMenuItem.Click += new System.EventHandler(this.cOM9ToolStripMenuItem_Click);
+            // 
+            // cOM10ToolStripMenuItem
+            // 
+            this.cOM10ToolStripMenuItem.Name = "cOM10ToolStripMenuItem";
+            this.cOM10ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.cOM10ToolStripMenuItem.Text = "COM10";
+            this.cOM10ToolStripMenuItem.Click += new System.EventHandler(this.cOM10ToolStripMenuItem_Click);
+            // 
+            // COM
+            // 
+            this.COM.AutoSize = true;
+            this.COM.Location = new System.Drawing.Point(1073, 465);
+            this.COM.Name = "COM";
+            this.COM.Size = new System.Drawing.Size(0, 17);
+            this.COM.TabIndex = 4;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(11, 94);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "On";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(536, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(537, 444);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Arduino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 494);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.COM);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.openserialport);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Arduino";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button openserialport;
+        private System.Windows.Forms.Label Info;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem portyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wybierzPortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM6ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM7ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM8ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM9ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOM10ToolStripMenuItem;
+        private System.Windows.Forms.Label COM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arduino
+{
+    public partial class Arduino : Form
+    {
+        public string port;
+
+        public Arduino()
+        {
+            InitializeComponent();
+            COM.Text = "Nie wybrano portu";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = port;
+            serialPort1.Open();
+            Info.Text = "Opened";
+        }
+
+        private void Info_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cOM1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM1";
+            COM.Text = port;
+        }
+
+        private void cOM2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM2";
+            COM.Text = port;
+        }
+
+        private void cOM3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM3";
+            COM.Text = port;
+        }
+
+        private void cOM4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM4";
+            COM.Text = port;
+        }
+
+        private void cOM5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM5";
+            COM.Text = port;
+        }
+
+        private void cOM6ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM6";
+            COM.Text = port;
+        }
+
+        private void cOM7ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM7";
+            COM.Text = port;
+        }
+
+        private void cOM8ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM8";
+            COM.Text = port;
+        }
+
+        private void cOM9ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM9";
+            COM.Text = port;
+        }
+
+        private void cOM10ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            port = "COM10";
+            COM.Text = port;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("0");
+        }
+
+        
+    }
+}
